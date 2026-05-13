31f8ae781655c73f3ebfd0306ddf7e82f9473d81
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2026-05-12 18:21:30 -0700

    datatypes.py: Update link to MQTT walkthrough to use the documentation site

--- src/qtpy_datalogger/datatypes.py
+++ src/qtpy_datalogger/datatypes.py
@@ -20,10 +20,10 @@ class Links(enum.StrEnum):
     """URLs for references and help."""
 
     Source = "https://github.com/wireddown/qtpy-datalogger"
-    Homepage = "https://downtothewire.io/qtpy-datalogger"
+    Homepage = "https://downtothewire.io/qtpy-datalogger/"
     New_Bug = "https://github.com/wireddown/qtpy-datalogger/issues/new?template=bug-report.md"
     Board_Support_Matrix = "https://docs.circuitpython.org/en/stable/shared-bindings/support_matrix.html"
-    MQTT_Walkthrough = "https://github.com/wireddown/qtpy-datalogger/wiki/Walkthrough-5-MQTT"
+    MQTT_Walkthrough = "https://wireddown.github.io/qtpy-datalogger/eng/intro/mqtt/"
 
 
 class ExitCode(enum.IntEnum):
